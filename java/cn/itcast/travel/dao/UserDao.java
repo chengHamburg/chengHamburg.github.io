2da31a4c70561d9889e1abf9526f9b79e98335e6
chengHamburg <18262456032@163.com>
2021-06-15 19:56:20 +0800

    fds

--- java/cn/itcast/travel/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.travel.dao;
-
-import cn.itcast.travel.domain.User;
-
-public interface UserDao {
-    boolean register(User user);
-    void save(User user);
-}
--- java/cn/itcast/travel/service/Usersevice.java
+++
@@ -1,7 +0,0 @@
-package cn.itcast.travel.service;
-
-import cn.itcast.travel.domain.User;
-
-public interface Usersevice {
-    boolean register1(User user);
-}
